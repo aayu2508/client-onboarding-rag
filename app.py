92aaac209f776cee4101f524affce94337b3046a
Aayushi <aayushi2508gupta@gmail.com>
2025-12-24 13:24:31 -0800

    retrieval + chromaDB

--- app.py
+++ app.py
@@ -1,9 +1,51 @@
 from data.employees import generate_employee_data
 import json
+from dotenv import load_dotenv
+import streamlit as st
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+from langchain_chroma import Chroma
+import logging
+# from assistant import Assistant
+# from prompts import SYSTEM_PROMPT, WELCOME_MESSAGE
+from langchain_groq import ChatGroq
+# from gui import AssistantGUI
 
 if __name__ == "__main__":
+    load_dotenv()
+    logging.basicConfig(level=logging.INFO)
+    st.set_page_config(page_title="Umbrella Onboarding", page_icon=":umbrella:", layout="wide")
 
-    # Here is an example of how to use the get_user_data function
-    users = generate_employee_data(1)[0]
-    print("\n\nUser data:")
-    print(json.dumps(users, indent=4))
+    # Generate employee data for a single user to send to our assistant
+    @st.cache_data(ttl=3600, show_spinner="Loading employee data...")
+    def get_user_data():
+        return generate_employee_data(1)[0]
+
+    # Initialize vector store for PDFs (embeddings created using OpenAI)
+    @st.cache_resource(ttl=3600, show_spinner="Loading Vector Store...")
+    def init_vector_store(pdfs_path):
+        try:
+            loader = PyPDFLoader(pdfs_path)
+            documents = loader.load()
+            text_splitter = RecursiveCharacterTextSplitter(chunk_size=4000, chunk_overlap=200)
+            splits = text_splitter.split_documents(documents)
+            embeddings = OpenAIEmbeddings()
+
+            # Persistent path for vector store
+            persistent_path = ".data/vectorstore"
+            vector_store = Chroma.from_documents(
+                splits,
+                embeddings,
+                persist_directory=persistent_path
+            )
+            return vector_store
+        except Exception as e:
+            logging.error(f"Error initializing vector store: {e}")
+            st.error(f"Error initializing vector store: {e}")
+            return None
+    
+    user_data = get_user_data()
+    vector_store = init_vector_store("data/umbrella_onboarding_guide.pdf")
+    
+    
---
+++ assistant.py
@@ -0,0 +1,47 @@
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
+
+class Assistant:
+    def __init__(
+        self,
+        system_prompt,
+        llm,
+        message_history=[],
+        vector_store=None,
+        user_information=None,
+    ):
+        self.system_prompt = system_prompt
+        self.llm = llm
+        self.messages = message_history
+        self.vector_store = vector_store
+        self.user_information = user_information
+        self.chain = self._get_conversation_chain()
+
+    def get_response(self, user_query):
+        return self.chain.invoke({
+            "user_input": user_query,
+        })
+
+    def _get_conversation_chain(self):
+        prompt = ChatPromptTemplate(
+            messages=[
+                ("system", self.system_prompt),
+                MessagesPlaceholder(variable_name="conversation_history"),
+                ("human", "{user_input}"),
+            ]
+        )
+
+        llm = self.llm
+        output_parser = StrOutputParser()
+        chain = ({
+            "retrieved_policy_information": self.vector_store.as_retriever(),
+            "user_information": lambda x: self.user_information,
+            "user_input": RunnablePassthrough(),
+            "conversation_history": lambda x: self.messages,
+        }
+            | prompt
+            | llm
+            | output_parser
+        )
+        return chain
